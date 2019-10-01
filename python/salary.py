2ed5b3ff002d89f04f994729fa2c9f74abd53485
SumanjaliDamarla <damarlasumanjali@gmail.com>
2019-10-01 23:34:06 +0530

    Added increment

--- python/salary.py
+++ python/salary.py
@@ -3,17 +3,18 @@ moves = []
 
 def changeList(salaries):
     max_sal = max(salaries)
+    min_sal = min(salaries)
     pos = salaries.index(max_sal)
     for i in range(len(salaries)):
         if i != pos:
-            salaries[i] = salaries[i] + 1
-    return salaries
+            salaries[i] = salaries[i] + (max_sal - min_sal)
+    return salaries, (max_sal - min_sal)
 
 def getNumberOfMoves(salaries):
     number_of_moves = 0
     while(len(set(salaries)) > 1):
-        salaries = changeList(salaries)
-        number_of_moves = number_of_moves + 1
+        salaries, increment = changeList(salaries)
+        number_of_moves = number_of_moves + increment
     return number_of_moves
 
 for i in range(T):
