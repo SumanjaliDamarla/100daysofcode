0034168f1ff43b6130a3996bbb26a66a0e9e0ae6
SumanjaliDamarla <damarlasumanjali@gmail.com>
2019-10-02 13:27:01 +0530

    Rainbow naive logic implemented

--- python/rainbowa.py
+++ python/rainbowa.py
@@ -1,6 +1,17 @@
 T = int(input())
+rainbow = list(range(1, 8))
+isRainbow = []
+def getIsRainbow(array):
+    is_rainbow = False
+    if array[::-1] == array and sorted(list(set(array))) == rainbow:
+        print(array)
+        is_rainbow = True
+    return is_rainbow
 
 for i in range(T):
     N = int(input())
     array = list(map(int, input().split()))
+    isRainbow.append("yes") if getIsRainbow(array) else isRainbow.append("no")
 
+for value in isRainbow:
+    print(value)
